e79854d5d752884be41f707eec8091072f1ecc54
Hugh Morrison <hmorri32@gmail.com>
2017-10-23 15:02:05 -0600

    model test category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Category, type: :model do
   describe 'validations' do
     it {should validate_presence_of(:title)}
   end
+
+  describe "relationships" do
+    it {should have_many(:ideas)}
+    it {should respond_to(:ideas)}
+  end
 end
