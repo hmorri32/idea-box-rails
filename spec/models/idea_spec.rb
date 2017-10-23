a4c7137a0fbebe3547002c9a962c801c6d22eb8d
Hugh Morrison <hmorri32@gmail.com>
2017-10-23 15:00:01 -0600

    configure shoulda

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,34 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe Idea, type: :model do
+RSpec.describe Idea, type: :model do  
   it 'should have a quality and it should default to 0' do
     idea = Idea.new
     expect(idea.quality).to eq(0)
   end
 
   describe 'validations' do
-    it 'should be invalid without title' do
-      category = Category.create(title: 'cool')
-      idea_sans_title = Idea.new(body: 'suh', category: category)
-      expect(idea_sans_title).to be_invalid
+    it {should validate_presence_of(:title)}
+    it {should validate_presence_of(:body)}
+
+    it 'should be invalid without a title and a body' do
+      idea_sans_erthang = Idea.new
+      expect(idea_sans_erthang).to be_invalid
     end
 
-    it 'should be invalid without body' do
+    it 'should be valid given all params' do
       category = Category.create(title: 'cool')
-      idea_sans_body = Idea.new(title: 'suh', category: category)
-      expect(idea_sans_body).to be_invalid
+      idea     = Idea.new(title: 'cool idea', body: 'totally', category: category)
+      expect(idea).to be_valid
     end
   end
 
-  it 'should be invalid without a title and a body' do
-    category = Category.create(title: 'cool')
-    idea_sans_erthang = Idea.new
-    expect(idea_sans_erthang).to be_invalid
-  end
-
-  it 'should be valid given all params' do
-    category = Category.create(title: 'cool')
-    idea = Idea.new(title: 'cool idea', body: 'totally', category: category)
-    expect(idea).to be_valid
+  describe "relationships" do
+    it {should belong_to(:category)}
+    it {should respond_to(:category)}
   end
 end
