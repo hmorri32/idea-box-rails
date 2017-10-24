48fb12a1b44b1199985f5ef1cb15089b66aa6d4d
Hugh Morrison <hmorri32@gmail.com>
2017-10-24 16:17:40 -0600

    slight refactor basecontroller

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,7 +1,7 @@
 class Admin::BaseController < ApplicationController
-  before_action :current_admin
+  before_action :require_admin
 
-  def current_admin
-    render file: 'public/404.html', status: 404, layout: false unless current_user.admin?
+  def require_admin
+    render file: 'public/404.html', status: 404, layout: false unless current_admin?
   end
 end
