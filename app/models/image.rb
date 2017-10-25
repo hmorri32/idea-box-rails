d8ba9369dfbc4eaf081ac13fe99dc541eb11eaf2
Hugh Morrison <hmorri32@gmail.com>
2017-10-25 09:56:18 -0600

    adds paperclip

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,11 @@
 class Image < ApplicationRecord
-  validates :url, presence: true
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
-  has_many :idea_images
-  has_many :ideas, through: :idea_images
+  has_attached_file :image, :styles => {:thumb  => "100x100#",
+                                        :small  => "150x150>",
+                                        :medium => "200x200" }
+
+  validates :url, presence: true
+  has_many  :idea_images
+  has_many  :ideas, through: :idea_images
 end
