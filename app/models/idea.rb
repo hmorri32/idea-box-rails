90a280430be8449be7e832609d298ae3ea31b5ea
Hugh Morrison <hmorri32@gmail.com>
2017-10-26 08:02:48 -0600

    add enum for quality to idea

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,7 +1,9 @@
 class Idea < ApplicationRecord
   validates  :title, :body, presence: true
-  belongs_to :category, optional: true
 
-  has_many :idea_images
-  has_many :images, through: :idea_images
+  belongs_to :category, optional: true
+  has_many   :idea_images
+  has_many   :images, through: :idea_images
+
+  enum :quality => ['junk', 'whatever', 'ultra cool']
 end
--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -3,14 +3,15 @@ require 'rails_helper'
 RSpec.describe Idea, type: :model do
   before :each { @category = build(:category) }
 
-  it 'should have a quality and it should default to 0' do
+  it 'should have a quality and it should default to 0(junk)' do
     idea = Idea.new
-    expect(idea.quality).to eq(0)
+    expect(idea.quality).to eq('junk')
   end
 
   describe 'validations' do
-    it {should validate_presence_of(:title)}
-    it {should validate_presence_of(:body)}
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:body) }
+    it { should define_enum_for(:quality) }
 
     it 'should be invalid without a title and a body' do
       idea_sans_erthang = Idea.new
@@ -24,8 +25,9 @@ RSpec.describe Idea, type: :model do
   end
 
   describe "relationships" do
-    it { should belong_to(:category)}
-    it { should respond_to(:category)}
+    it { should belong_to(:category) }
+    it { should respond_to(:category) }
     it { should have_many(:images).through(:idea_images) }
+    it { should have_many(:idea_images) }
   end
 end
